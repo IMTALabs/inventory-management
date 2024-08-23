63ef2164913f4c4f3f2c66fdde5e88df5de9395f
code-tieumomo <code.tieumomo@gmail.com>
2024-08-23 17:21:51 +0700

    fix: current user role

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -84,7 +84,7 @@
                                     name="role">
                                 @foreach(\App\Enums\RoleEnum::cases() as $availableRole)
                                     <option value="{{ $availableRole->value }}"
-                                            @if(old('role') === $availableRole->value) selected @endif>
+                                            @if($user->role->value === $availableRole->value) selected @endif>
                                         {{ ucfirst($availableRole->value) }}
                                     </option>
                                 @endforeach
